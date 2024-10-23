47800f42155f56100db6e13251e8942b7d38de6f
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-23 13:02:39 +0000

    troubleshooting heroku deployment

--- elite/settings.py
+++ elite/settings.py
@@ -28,10 +28,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-danparkinso-project4eli-t3aevo71tlq.ws.codeinstitute-ide.net',
-                '.herokuapp.com',]
+                'herokuapp.com',
+                'elite-reservations.herokuapp.com',]
 
 
 # Application definition
