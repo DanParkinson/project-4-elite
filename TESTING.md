76967aa651ca38a3f88988fad0882246ebc0dc84
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-23 13:39:26 +0000

    TESTING.md SuperUser

--- TESTING.md
+++ TESTING.md
@@ -5,3 +5,22 @@ A variety of exploratory tests were performed throughout the project.
 ## MANUAL TESTING
 
 Getting the live deployed site working. This is well documented in the latter sections of [DEPLOYMENT.md](DEPLOYMENT.md)
+
+## Super User
+
+<details>
+<summary>Click me</summary>
+
+When i originally tried to login as a superuser i recieved this error.
+
+![Error](docs/testing/1superuser01.png)
+
+After using code institute support i had forgotten to add.
+
+![CSFR](docs/testing/1superuser02.png)
+
+Admin login now functions correctly.
+
+![Admin Page](docs/testing/1superuser03.png)
+
+</details>
--- elite/settings.py
+++ elite/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-danparkinso-project4eli-t3aevo71tlq.ws.codeinstitute-ide.net',
                 'herokuapp.com',
@@ -136,3 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-danparkinso-project4eli-t3aevo71tlq.ws.codeinstitute-ide.net']
