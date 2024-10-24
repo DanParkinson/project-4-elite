22720b88a29c3616403c54ee491b8900c8e7aa48
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-24 12:04:58 +0000

    Users can now view their account information

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required # Users must be logged on to access this view
 def my_account(request):
-    return render(request, 'profiles/my_account.html')
+    user = request.user
+    context = {
+        'username': user.username,
+        'email': user.email
+    }
+    return render(request, 'profiles/my_account.html', context)
