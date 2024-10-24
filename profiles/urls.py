f125a2281c3c70439fa01b8aa7e356149687bfbe
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-24 12:26:23 +0000

    Users can now update their account details

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my-account/', views.my_account, name='my_account'),  # URL to access the account page
+    path('my-account/', views.my_account, name='my_account'),
+    path('update-account/', views.update_account, name='update_account')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm
 
 @login_required # Users must be logged on to access this view
 def my_account(request):
@@ -9,3 +10,14 @@ def my_account(request):
         'email': user.email
     }
     return render(request, 'profiles/my_account.html', context)
+
+def update_account(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("my_account")
+    else:
+        form = UserUpdateForm(instance=request.user)
+        
+    return render(request, 'profiles/update_account.html', {'form': form})
