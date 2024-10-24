42b4f4fb9ef03fb694b7ac4a646d8792c7fcc367
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-24 12:47:14 +0000

    Users can now change their passwords

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('my-account/', views.my_account, name='my_account'),
-    path('update-account/', views.update_account, name='update_account')
+    path('update-account/', views.update_account, name='update_account'),
+    path('update-password/', views.update_password, name='update_password'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserUpdateForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+
 
 @login_required # Users must be logged on to access this view
 def my_account(request):
@@ -10,6 +13,7 @@ def my_account(request):
         'email': user.email
     }
     return render(request, 'profiles/my_account.html', context)
+
 
 def update_account(request):
     if request.method == 'POST':
@@ -21,3 +25,18 @@ def update_account(request):
         form = UserUpdateForm(instance=request.user)
         
     return render(request, 'profiles/update_account.html', {'form': form})
+
+
+def update_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, form.user)  # Important to keep the user logged in
+            return redirect('my_account')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+    return render(request,  'profiles/update_password.html', {'form': form})
