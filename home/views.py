14d1255577fa6ed6010a6c57bf97bd7503c8d09d
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-27 16:13:41 +0000

    next_reservation now checks datetime to filter off past reservations

--- home/views.py
+++ home/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from reservations.models import Reservation
 from django.utils import timezone
+from datetime import datetime
 
+# shows a users next reservation on the homepage
 def index(request):
     next_reservation = None # for users without a reservation
     if request.user.is_authenticated: # check if logged in
-        next_reservation = Reservation.objects.filter(
-            user = request.user, # get users reservations
-            reservation_date__gte = timezone.now().date(), # get the future reservations
-        ).order_by('reservation_date', 'reservation_time').first() # get the first reservation
+        now = timezone.now() # current date and time for removing reservations today that time has expired
+        future_reservations = Reservation.objects.filter(user = request.user) # get users reservations
+
+        # filter out past reservations
+        for reservation in future_reservations:
+            reservation_datetime = timezone.make_aware(
+                datetime.combine(reservation.reservation_date, reservation.reservation_time)
+            )
+            # 
+            if reservation_datetime >= now:
+                next_reservation = reservation
+                break # get the first valid future reservation
 
     return render(request, 'index.html', {'next_reservation' : next_reservation}) # loads index.html on request
 
