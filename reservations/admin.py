3a2acb03322825910dcd85aedc6ecde3f66f716b
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-27 11:45:51 +0000

    admin page now shows reservation model

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Reservation
 
 # Register your models here.
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'phone_number','number_of_guests','reservation_date','reservation_time','special_occasion')
