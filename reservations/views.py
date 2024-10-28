b06fbc28b844be1ac2d31eed24f465784baeba9d
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-28 16:55:08 +0000

    make reservations can give available times now?

--- reservations/views.py
+++ reservations/views.py
@@ -42,23 +42,47 @@ def make_reservation(request):
                 )
             )
 
-            # check is the overlapping reservation exists
-            # new reservation is not saved and error is thrown
+            # check if the overlapping reservation exists
+            # This is for showing the user available times for the chosen day
             if overlapping_reservation.exists():
                 # Generate all the times on the given day in the opening hours window
+                all_times = [
+                    reservation_datetime.replace(hour = h, minute = m)
+                    for h in range(10, 21)
+                    for m in range(0, 60, 15)
+                ]
+                
                 # Get all exisiting reservations on the day chosen
+                chosen_day_reservations = Reservation.object.filter(
+                    reservation_date = reservation_date
+                )
+
                 # remove all times that are with 1:45 of each reservation
+                for time in all_times:
+                    # this checks the reservations buffer zone of 2 hours.
+                    # IF NOT means that if a time IS available. append it to available_times
+                    if not chosen_day_reservations.filter(
+                        # for each reservation time add a buffer of 1hour 45 mins
+                        reservation_time__range = (
+                            time - timedelta(hours=1, minutes=45),
+                            time + timedelta(hours=1, minutes=45),
+                        )
+                        # if they exist append them to available times
+                        ).exists():
+                            available_times.append(time.strftime("%H:%M"))
 
+            # Error message to tell user that the time is unavailable
+            form.add_error(None, "This reservation is unavaiable")
 
-                # Error message to tell user that the time is unavailable
-                form.add_error(None, "This reservation is unavaiable")
+        # if form is valid else
+        else:
+            # new reservation is okay and is saved
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
+            return redirect('home')
 
-            else:
-                # new reservation is okay and is saved
-                reservation = form.save(commit=False)
-                reservation.user = request.user
-                reservation.save()
-                return redirect('home')
+    # if request method else
     else:
         form = ReservationForm() # If loading the page, load the form
     return render(request, 'reservations/make_reservation.html', {'form': form})
